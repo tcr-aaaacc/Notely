ac7c91d2dd34177b5e7246732455da42bfd7bb52
Chinedu Ogbu <techsafety2010@yahoo.com>
2019-07-06 19:52:51 +0100

    Commit number 1: Our first  commit

--- NotelyApp/Controllers/HomeController.cs
+++ NotelyApp/Controllers/HomeController.cs
@@ -1,33 +1,96 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using NotelyApp.Models;
+using NotelyApp.Repositories;
 
 namespace NotelyApp.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly INoteRepository _noteRepository;
+        public HomeController(INoteRepository noteRepository)
+        {
+            _noteRepository = noteRepository;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var notes = _noteRepository.GetAllNotes().Where(n => n.IsDeleted == false);
+
+            return View(notes);
         }
 
-        public IActionResult About()
+        public IActionResult NoteDetail(Guid id)
         {
-            ViewData["Message"] = "Your application description page.";
+            var note = _noteRepository.FindNoteById(id);
 
-            return View();
+            return View(note);
         }
 
-        public IActionResult Contact()
+        [HttpGet]
+        public IActionResult NoteEditor(Guid id = default)
         {
-            ViewData["Message"] = "Your contact page.";
+            if (id != Guid.Empty)
+            {
+                var note = _noteRepository.FindNoteById(id);
+
+                return View(note);
+            }
 
             return View();
+
         }
+
+        [HttpPost]
+        public IActionResult NoteEditor(NoteModel noteModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var date = DateTime.Now;
+
+                if (noteModel != null && noteModel.Id == Guid.Empty)
+                {
+                    noteModel.Id = Guid.NewGuid();
+                    noteModel.CreatedDate = date;
+                    noteModel.LastModified = date;
+
+                    _noteRepository.SaveNote(noteModel);
+                }
+                else
+                {
+                    var note = _noteRepository.FindNoteById(noteModel.Id);
+                    note.LastModified = date;
+                    note.Subject = noteModel.Subject;
+                    note.Detail = noteModel.Detail;
+                }
+
+                return RedirectToAction("Index"); 
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public IActionResult DeleteNote(Guid id)
+        {
+            var note = _noteRepository.FindNoteById(id);
+
+            note.IsDeleted = true;
+
+            return RedirectToAction("Index");
+        }
+
+
+
+
+
+
+
+
+
 
         public IActionResult Privacy()
         {
--- NotelyApp/Startup.cs
+++ NotelyApp/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using NotelyApp.Repositories;
 
 namespace NotelyApp
 {
@@ -32,6 +33,8 @@ namespace NotelyApp
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSingleton<INoteRepository, NoteRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
